d67c1dbf5667e26d9daba26d79a85953d8ebc896
manoaiharivolaa <manoaiharivola@gmail.com>
2024-04-04 22:16:55 +0300

    Création du projet

---
+++ src/main/java/mg/manoa/iharivola/tp2prgjsfmanoaiharivola19/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package mg.manoa.iharivola.tp2prgjsfmanoaiharivola19;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
